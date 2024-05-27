3f605c27ee6c420f953b86ab62c28f958206f187
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-05-27 12:04:13 +0800

    CHARTS

--- API/urls.py
+++ API/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from API import views
 
 urlpatterns = [ 
-            path('barangay_data', views.barangay_data, name = "barangay data")
+            path('barangay_data', views.barangay_data, name = "barangay data"),
+            path('age_data', views.age_data, name = "age data"),
+            path('family_voters_data', views.family_voters_data, name = "family voters data"),
                ]
---
+++ LandingPage/static/charts.js
@@ -0,0 +1,115 @@
+
+const ctx = document.getElementById("myChart")
+
+fetch("/api/barangay_data")
+  .then((response) => response.json())
+  .then((data) => {
+    const jsonData = JSON.parse(data)
+    const labels = Object.keys(jsonData)
+    const counts = Object.values(jsonData)
+    console.log(data)
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels,
+
+        datasets: [
+          {
+            label: "# of Registered Members",
+            data: counts,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        resonsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: "Registered BH Members",
+          },
+        },
+      },
+    })
+  })
+
+// Age Chart
+
+const ageChart = document.getElementById("ageChart")
+
+fetch("/api/age_data")
+  .then((response) => response.json())
+  .then((data) => {
+    const jsonData = JSON.parse(data)
+    const labels = Object.keys(jsonData)
+    const counts = Object.values(jsonData)
+    console.log(data)
+
+    new Chart(ageChart, {
+      type: "pie",
+      data: {
+        labels: labels,
+
+        datasets: [
+          {
+            label: "Age Group",
+            data: counts,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        resonsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: "Demographics of Registered BH Members by Age",
+          },
+        },
+      },
+    })
+  })
+
+
+
+
+
+  
+const familyVotersChart = document.getElementById("familyVotersChart")
+
+fetch("/api/family_voters_data")
+  .then((response) => response.json())
+  .then((data) => {
+    const jsonData = JSON.parse(data)
+    const labels = Object.keys(jsonData)
+    const counts = Object.values(jsonData)
+    console.log(data)
+
+    new Chart(familyVotersChart, {
+      type: "pie",
+      data: {
+        labels: labels,
+
+        datasets: [
+          {
+            label: "Age Group",
+            data: counts,
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        resonsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: "Family Voters Count of Registered BH Members",
+          },
+        },
+      },
+    })
+  })
