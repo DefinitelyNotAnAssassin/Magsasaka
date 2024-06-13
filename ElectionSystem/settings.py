a4d0b2428ec2bce4639713095593a24c8b0fc75d
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-06-14 06:39:33 +0800

    Configuration for Pythonanywhere

--- ElectionSystem/settings.py
+++ ElectionSystem/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'bagonghenerasyon.pythonanywhere.com',
+    'magsasaka.pythonanywhere.com',
 ]
 
 
--- UserModule/views.py
+++ UserModule/views.py
@@ -11,12 +11,12 @@ def virtual_id(request):
       signature = "https://cdn.wallpapersafari.com/15/64/s0zmcy.jpg"
 
     try:
-      photo = request.user.photo.url.replace('UserModule/', '')
+      photo = request.user.photo.url
     except Exception as e:
       photo = "https://cdn.wallpapersafari.com/15/64/s0zmcy.jpg"
 
     try:
-      qr_code = request.user.qr_code.url.replace('UserModule/', '')
+      qr_code = request.user.qr_code.url
     except Exception as e:
       qr_code = "https://cdn.wallpapersafari.com/15/64/s0zmcy.jpg"
 
